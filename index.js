d03672fbed3e4e6b94f7322b4e4c3ad9201b462b
Akshay Thota <akshay.thota@juspay.in>
2023-06-09 00:39:29 +0530

    first blood

---
+++ index.js
@@ -0,0 +1,27 @@
+// Requiring necessary modules
+const express = require('express');
+const fs = require('fs');
+const app = express();
+
+// Defining get request at '/' route
+app.get('/', function(req, res) {
+  // Read JSON data from file
+  fs.readFile('data.json', 'utf8', function(err, data) {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Internal Server Error');
+      return;
+    }
+    
+    // Parse the JSON data
+    const jsonData = JSON.parse(data);
+    
+    // Send the JSON response
+    res.json(jsonData);
+  });
+});
+
+// Setting the server to listen at port 3000
+app.listen(3000, function(req, res) {
+  console.log("Server is running at port 3000");
+});
